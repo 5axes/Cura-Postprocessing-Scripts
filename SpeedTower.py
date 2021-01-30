1a6e35a4c075bb63a789866c4d5d388f02ed4b87
5axes <5axes@users.noreply.github.com>
2021-01-30 18:44:33 +0100

    Update SpeedTower.py

--- SpeedTower.py
+++ SpeedTower.py
@@ -1,12 +1,16 @@
+#------------------------------------------------------------------------------
 # Cura PostProcessingPlugin
 # Author:   5axes
 # Date:     February 29, 2020
 #
-# Description:  postprocessing-script to easily define a Speed Tower
-#
+# Description:  postprocessing-script to easily define a Speed Tower 
+#           Option for  Speed
+#                       Acceleration
+#                       Junction Deviation
 #
 #   Version 1.0 29/02/2020
-#
+#   Version 1.1 29/01/2021
+#------------------------------------------------------------------------------
 
 from ..Script import Script
 from UM.Logger import Logger
@@ -32,7 +36,8 @@ class SpeedTower(Script):
                     "type": "enum",
                     "options": {
                         "acceleration": "Acceleration",
-                        "jerk": "Jerk"
+                        "jerk": "Jerk",
+                        "junction": "Junction Deviation"
                     },
                     "default_value": "acceleration"
                 },
@@ -40,21 +45,21 @@ class SpeedTower(Script):
                 {
                     "label": "Starting value",
                     "description": "the starting value of the Tower.",
-                    "type": "int",
-                    "default_value": 8
+                    "type": "float",
+                    "default_value": 8.0
                 },
                 "valueChange":
                 {
                     "label": "Value Increment",
-                    "description": "the value change of each block, can be positive or negative. I you want 220 and then 210, you need to set this to -10.",
-                    "type": "int",
-                    "default_value": 4
+                    "description": "the value change of each block, can be positive or negative. I you want 50 and then 40, you need to set this to -10.",
+                    "type": "float",
+                    "default_value": 4.0
                 },
                 "changelayer":
                 {
                     "label": "Change Layer",
                     "description": "how many layers needs to be printed before the value should be changed.",
-                    "type": "float",
+                    "type": "int",
                     "default_value": 30,
                     "minimum_value": 1,
                     "maximum_value": 1000,
@@ -65,20 +70,28 @@ class SpeedTower(Script):
                 {
                     "label": "Change Layer Offset",
                     "description": "if the Tower has a base, put the layer high off it here",
-                    "type": "float",
-                    "default_value": 10,
+                    "type": "int",
+                    "default_value": 4,
                     "minimum_value": 0,
                     "maximum_value": 1000,
                     "maximum_value_warning": 100
-                }
+                },
+                "lcdfeedback":
+                {
+                    "label": "Display details on LCD?",
+                    "description": "This setting will insert M117 gcode instructions, to display current junction deviation value is being used.",
+                    "type": "bool",
+                    "default_value": true
+                }                
             }
         }"""
 
     def execute(self, data):
 
+        UseLcd = self.getSettingValueByKey("lcdfeedback")
         Instruction = self.getSettingValueByKey("command")
-        StartValue = self.getSettingValueByKey("startValue")
-        ValueChange = self.getSettingValueByKey("valueChange")
+        StartValue = float(self.getSettingValueByKey("startValue"))
+        ValueChange = float(self.getSettingValueByKey("valueChange"))
         ChangeLayer = self.getSettingValueByKey("changelayer")
         ChangeLayerOffset = self.getSettingValueByKey("changelayeroffset")
         ChangeLayerOffset += 1  # Modified to take into account the numbering offset in Gcode
@@ -101,20 +114,35 @@ class SpeedTower(Script):
                     if (layer_index==ChangeLayerOffset):
                         if  (Instruction=='acceleration'):
                             Command = "M204 S{:d}".format(int(CurrentValue))
+                            lcd_gcode = "M117 Acceleration S{:d}".format(int(CurrentValue))
                         if  (Instruction=='jerk'):
                             Command = "M205 X{:d} Y{:d}".format(int(CurrentValue), int(CurrentValue))
+                            lcd_gcode = "M117 Jerk X{:d} Y{:d}".format(int(CurrentValue), int(CurrentValue))
+                        if  (Instruction=='junction'):
+                            Command = "M205 J{:.3f}".format(float(CurrentValue))
+                            lcd_gcode = "M117 Junction J{:.3f}".format(float(CurrentValue))         
+                            
                         lines.insert(line_index + 1, ";TYPE:CUSTOM LAYER")
                         lines.insert(line_index + 2, Command)
+                        if UseLcd == True :               
+                            lines.insert(line_index + 3, lcd_gcode)
 
                     if ((layer_index-ChangeLayerOffset) % ChangeLayer == 0) and ((layer_index-ChangeLayerOffset)>0):
                             CurrentValue += ValueChange
                             if  (Instruction=='acceleration'):
                                 Command = "M204 S{:d}".format(int(CurrentValue))
+                                lcd_gcode = "M117 Acceleration S{:d}".format(int(CurrentValue))
                             if  (Instruction=='jerk'):
                                 Command = "M205 X{:d} Y{:d}".format(int(CurrentValue), int(CurrentValue))
+                                lcd_gcode = "M117 Jerk X{:d} Y{:d}".format(int(CurrentValue), int(CurrentValue))
+                            if  (Instruction=='junction'):
+                                Command = "M205 J{:.3f}".format(float(CurrentValue))
+                                lcd_gcode = "M117 Junction J{:.3f}".format(float(CurrentValue))
+                                
                             lines.insert(line_index + 1, ";TYPE:CUSTOM VALUE")
                             lines.insert(line_index + 2, Command)
-                                               
+                            if UseLcd == True :               
+                               lines.insert(line_index + 3, lcd_gcode)                                              
             
             result = "\n".join(lines)
             data[layer_index] = result
